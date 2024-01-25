06e72b68d468d608d3c28da5fcb665068e6be63d
Admin <Admin@DESKTOP-5NKFECA>
2024-01-25 15:20:05 +0200

    first commit

--- Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
+++ Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
@@ -21,33 +21,37 @@ class TriangleClassifier {
 	
 	public static void classifyTriangle(int a, int b, int c) {
 			
-	        
+		if(isTriangle(a, b, c)){//בודקים בהתחלה אם הוא משולש בכלל
 	        if (isEquilateral(a, b, c)) {
-	        	triangle= Triangle_Types.Scalene;
+	        	triangle= Triangle_Types.equilateral;
 	        }else if(isIsosceles(a, b, c)) {
-	        	triangle= Triangle_Types.Nottriangle;
-	        }else if(isTriangle(a, b, c)){
 	        	triangle= Triangle_Types.isosceles;
+	        }else if(isScalene(a, b, c)){
+	        	triangle= Triangle_Types.Scalene;
 	        }else {
 	        	
-	        	triangle=Triangle_Types.Scalene;
+	        	triangle=Triangle_Types.ERROR;
 	        }
-
-	       
+		}else//אם לא משולש אז מחזרים לא משולש
+			triangle=Triangle_Types.Nottriangle;
 	    }
+	
+	 
 
 	    private static boolean isTriangle(int a, int b, int c) {
-	        return b + b > c && a + c > b && b + c > a;
+	        return a + b > c && a + c > b && b + c > a;
 	    }
-
+	    
+	    private static boolean isScalene(int a, int b, int c) {
+	    	return a!=b && b!=c && a!=c;
+	    }
+	    
 	    private static boolean isIsosceles(int a, int b, int c) {
-	        return a + b > c || b + c > b && b + c > a && a == b && b == c;
+	        return ((a==b && b!=c) || (b==c && c!=a) || (a==c && c!=b));
 	    }    
 	    	
-
-
 	    private static boolean isEquilateral(int a, int b, int c ) {
-	    	return a + b > c && a + c > b && b + c > a && (a == a) || (b == c) || (c == a);
+	    	return  (a == b) && (b == c) && (c == a);
 	    }
 	   
 	    
--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -13,24 +13,48 @@ public class AppTest {
 	TriangleClassifier triangle = new TriangleClassifier();
 	
 	@Test
-  
+  //שווה צלעות
 	public void equilateralTriangle(){
-        int a = 2;
-        int b = 2;
-        int c=2;
+        int a = 8;
+        int b = 8;
+        int c=8;
         triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.isosceles.toString();
+        String expected =Triangle_Types.equilateral.toString();
         String result = triangle.get_type().toString();
-        //assertEquals(expected, result.toString());
+        org.junit.Assert.assertEquals(expected, result.toString());
     }
 	@Test
+	//שונה צלעות
 	public void check_Scalene(){
-        int a = 2;
-        int b = 2;
-        int c=4;
+        int a = 3;
+        int b = 4;
+        int c=5;
         triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.Nottriangle.toString();
+        String expected =Triangle_Types.Scalene.toString();
         String result = triangle.get_type().toString();
-        assertEquals(expected, result.toString());
+        org.junit.Assert.assertEquals(expected, result.toString());
     }
+	
+	@Test
+	//שווה שוקיים
+	public void check_isosceles(){
+		int a=8;
+		int b=5;
+		int c=8;
+		triangle.classifyTriangle(a, b, c);
+		String expected=Triangle_Types.isosceles.toString();
+		String result=triangle.get_type().toString();
+        org.junit.Assert.assertEquals(expected, result.toString());		
+	}
+	@Test
+	//תקינות משולש
+	public void check_IsTriangle(){
+		int a=3;
+		int b=5;
+		int c=8;
+		triangle.classifyTriangle(a, b, c);
+		String expected=Triangle_Types.Nottriangle.toString();
+		String result=triangle.get_type().toString();
+        org.junit.Assert.assertEquals(expected, result.toString());		
+	}
 }  
